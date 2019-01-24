bed1d10a943a8602aa5eb190b62a3857ffa73517
SunDi3yansyah <sundi3yansyah@gmail.com>
2019-01-24 22:47:05 +0700

    update contrib and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,11 @@ if [[ -z $1  ]]; then
 	echo "Option not set!";
 else
 	if [[ $1 != "master" ]]; then
-		if [[ $1 != "stable" ]]; then
-			rake guides:generate:html ALL=1 RAILS_VERSION=$1
-		else
-			rake guides:generate:html ALL=1 RAILS_VERSION=$1 STABLE=$2
+		if [[ $1 == "stable" ]]; then
+		  rake guides:generate:html ALL=1 RAILS_VERSION=$1 STABLE=$2
 		fi
-		git clone $REMOTE_FOR_DEPLOY repository && cp output/guides/$1/* repository -R
+		rake guides:generate:html ALL=1 RAILS_VERSION=$1
+		git clone $REMOTE_FOR_DEPLOY repository && cp output/guides/* repository -R
 		cd repository && git add --all && git commit -m "Deploy!"
 		git push origin master
 	else
