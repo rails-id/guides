021478857aa9bfdcf395509cf0a6a326c0f1aafe
SunDi3yansyah <sundi3yansyah@gmail.com>
2019-01-24 23:09:16 +0700

    update deploy kindle, CONTRIBUTING.md

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,21 @@
 if [[ -z $1  ]]; then
 	echo "Option not set!";
 else
-	if [[ $1 != "master" ]]; then
-		if [[ $1 != "stable" ]]; then
-			rake guides:generate:html ALL=1 RAILS_VERSION=$1
-		else
-			rake guides:generate:html ALL=1 RAILS_VERSION=$1 STABLE=$2
-		fi
-		git clone $REMOTE_FOR_DEPLOY repository && cp output/guides/$1/* repository -R
-		cd repository && git add --all && git commit -m "Deploy!"
-		git push origin master
-	else
+	if [[ $1 == "master" ]]; then
 		rake guides:generate:html ALL=1
 		git clone $REMOTE_FOR_DEPLOY repository && cp output/edge/* repository -R
+		cd repository && git add --all && git commit -m "Deploy!"
+		git push origin master
+
+	else
+	  setupkindlerb
+		if [[ $2 == "stable" ]]; then
+		  rake guides:generate:html ALL=1 RAILS_VERSION=$1 STABLE=1
+		  rake guides:generate:kindle ALL=1 RAILS_VERSION=$1 STABLE=1
+		fi
+		rake guides:generate:html ALL=1 RAILS_VERSION=$1
+		rake guides:generate:kindle ALL=1 RAILS_VERSION=$1
+		git clone $REMOTE_FOR_DEPLOY repository && cp output/guides/* repository -R
 		cd repository && git add --all && git commit -m "Deploy!"
 		git push origin master
 	fi
