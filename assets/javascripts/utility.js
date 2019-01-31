bff935b2680ba9305c2830feda7588ff26eb1b93
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-01 01:45:09 +0700

    Add button contribute to github

---
+++ assets/javascripts/utility.js
@@ -0,0 +1,3 @@
+$('#feedback').prepend("<section class=\"contribute-to-github\">\n" +
+  "  <a href=\"https://github.com/rails-id/guides\" target=\"_blank\">Kontribusi panduan ini di GitHub</a>\n" +
+  "</section>");
