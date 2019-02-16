c23f0f04e232fe4f435cd752e0159ce49efab3b0
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-16 20:39:51 +0700

    Hapus Section: Script Tambahan

--- assets/javascripts/utility.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("turbolinks:load", function() {
-  $('#feedback').prepend("<section class=\"contribute-to-github\">\n" +
-    "  <a href=\"https://github.com/rails-id/guides\" target=\"_blank\">Kontribusi panduan ini di GitHub</a>\n" +
-    "</section>");
-})
