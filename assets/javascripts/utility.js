334fd76ffd8df588d02b67c963ab20ed7790a907
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-16 20:44:05 +0700

    Hapus: Kontribusi panduan ini di GitHub, rubah pakai github corner

--- assets/javascripts/utility.js
+++
@@ -1,3 +0,0 @@
-$('#feedback').prepend("<section class=\"contribute-to-github\">\n" +
-  "  <a href=\"https://github.com/rails-id/guides\" target=\"_blank\">Kontribusi panduan ini di GitHub</a>\n" +
-  "</section>");
